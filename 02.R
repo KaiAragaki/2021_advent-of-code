9e5f20e9540fb769dbfdad05064e0e48747a4e07
KaiAragaki <adamaragaki@gmail.com>
2021-12-02 09:44:09 -0500

    add day 2 solutions

---
+++ 02.R
@@ -0,0 +1,22 @@
+df <- read.delim("data/02.txt", header = F, sep = " ", col.names = c("name", "v"))
+library(dplyr)
+library(tidyr)
+library(purrr)
+df |> 
+  group_by(name) |> 
+  summarise(value = sum(v)) |> 
+  pivot_wider() |> 
+  mutate(ans = (down - up) * forward)
+
+# Part 2 -----------------------------------------------------------------------
+
+df2 <- df |> 
+  mutate(aim = case_when(name == "forward" ~ 0L,
+                         name == "down" ~ v,
+                         name == "up" ~ -v) |> 
+           accumulate(sum),
+         fwd = ifelse(name == "forward", v, 0),
+         depth = fwd*aim)
+sum(df2$fwd)*sum(df2$depth)
+
+         
