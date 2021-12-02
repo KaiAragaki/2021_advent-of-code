7a8d29f60a8163c0cc88b74d50563787b17a50c2
KaiAragaki <adamaragaki@gmail.com>
2021-12-02 09:02:24 -0500

    rename data, new day script

--- 01.R
+++ 01.R
@@ -3,4 +3,5 @@ df <- read.csv(header = F, "data/day_01.1.txt")
 sum(df$V1 - dplyr::lag(df$V1) > 0, na.rm = T)
 
 sums <- slider::slide_dbl(df$V1, sum, .before = 1, .after = 1, .complete = T)
+
 sum(sums - dplyr::lag(sums) > 0, na.rm = T)
